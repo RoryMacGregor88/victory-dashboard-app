e0c3b94ec69b7d489432d16ab7aac2d6032c0c0f
Rory MacGregor <rory.macgregor@astrosat.net>
2023-06-30 01:13:28 +0100

    feat(frontend): Import theme from Orbis, styles now working.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import store from './store';
 import App from './App.tsx';
+import theme from './themes/index.ts';
 
 const { worker } = await import('./mocks/browser.ts');
 worker.start();
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <CssBaseline>
       <Provider store={store}>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </Provider>
     </CssBaseline>
   </React.StrictMode>
