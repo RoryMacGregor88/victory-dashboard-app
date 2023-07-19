3558072df68ba96de4066756fc99c848c2018a86
Rory MacGregor <rory.macgregor@astrosat.net>
2023-07-19 22:54:23 +0100

    feat(frontend): Remove top-level await

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,9 @@ import store from './store';
 import App from './App.tsx';
 import theme from './themes/index.ts';
 
+import { worker } from './mocks/browser.ts';
+
 /** mock server used to mimic the real backend that the dashboard uses in real life */
-const { worker } = await import('./mocks/browser.ts');
 worker.start();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
